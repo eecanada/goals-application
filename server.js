f6c33d73b2e74d44aac8498083c292e0938fdf43
Eder Canada <canada.eder@gmail.com>
2019-10-28 15:50:46 -0700

    set up body-parser

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 
 
 //controllers
-const usersController = require('./controllers/users')
+const usersController = require('./controllers/users');
 
 //uses
-app.use('/users',usersController)
+app.use('/users',usersController);
+app.use(methodOverride('_method'));
 
 //my home page 
 app.get('/', (req,res)=>{
   res.render('index.ejs')
-})
+});
 
 //my server
 app.listen(3000, ()=>{
   console.log(3000, 'server is listening')
-})
+});
