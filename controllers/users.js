d674804d7a47bea437e7ef8656ce38588717fa00
Eder Canada <canada.eder@gmail.com>
2019-10-28 16:02:11 -0700

    create post route for user

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/users.js');
 
 
 //1.0
@@ -11,5 +12,18 @@ router.get('/', (req,res)=>{
 router.get('/new', (req,res)=>{
 res.render('users/new.ejs')
 });
+
+
+//1.2
+router.post('/', (req,res)=>{
+  User.create(req.body, (err,createdUser)=>{
+    if(err){
+      console.log(err);
+    }else{
+      res.redirect('/users')
+    }
+  });
+});
+
 
 module.exports = router; 
