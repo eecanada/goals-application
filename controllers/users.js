3ce71d2308bcd75a43c37b81762724ecc54c9f02
Eder Canada <canada.eder@gmail.com>
2019-10-28 15:26:35 -0700

    create route link to new.ejs

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 
-
+//1.0
 router.get('/', (req,res)=>{
   res.render('users/index.ejs')
-})
+});
 
+//1.1
+router.get('/new', (req,res)=>{
+res.render('users/new.ejs')
+});
 
 module.exports = router; 
