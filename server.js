22ab013515272846963c62f8433d9f797f61f8c2
Eder Canada <canada.eder@gmail.com>
2019-10-28 14:58:20 -0700

    add to user index ejs page, update user controller on server.js

--- server.js
+++ server.js
@@ -3,19 +3,18 @@ const app = express();
 
 
 
+//controllers
+const usersController = require('./controllers/users')
 
+//uses
+app.use('/users',usersController)
 
-
+//my home page 
 app.get('/', (req,res)=>{
   res.render('index.ejs')
 })
 
-
-
-
-
-
-
+//my server
 app.listen(3000, ()=>{
   console.log(3000, 'server is listening')
 })
