2a00fb4e669c0e94ecf29c4f5c89ffb4fda2f657
Eder Canada <canada.eder@gmail.com>
2019-10-28 15:16:46 -0700

    create new.ejs page for user

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/users.js')
 
 
 
+router.get('/', (req,res)=>{
+  res.render('users/index.ejs')
+})
 
 
-module.exports = router;
+module.exports = router; 
