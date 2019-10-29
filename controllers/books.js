5e343ced50dc58639a7a8cb050a49edc26be7ac4
Eder Canada <canada.eder@gmail.com>
2019-10-29 09:16:33 -0700

    create new route for books

--- controllers/books.js
+++ controllers/books.js
@@ -2,4 +2,16 @@ const express = require('express');
 const router = express.Router();
 const Book = require('../models/books.js')
 
+
+//1.0
+router.get('/', (req,res)=>{
+  res.render('users/index.ejs')
+});
+
+
+//1.1
+router.get('/new', (req,res)=>{
+  res.render('books/new.ejs')
+  });
+
 module.exports = router;
