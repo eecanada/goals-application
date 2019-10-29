81afd81bbf4ff0a5fe85a4e033086d8221db78ae
Eder Canada <canada.eder@gmail.com>
2019-10-29 10:00:12 -0700

    create get route for show page

--- controllers/books.js
+++ controllers/books.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Book = require('../models/books.js')
 
-
 //1.0
 router.get('/', (req,res)=>{
   Book.find({}, (err, AllBooks)=>{
@@ -12,22 +11,31 @@ router.get('/', (req,res)=>{
   })
 });
 
-
-
-
 //1.1
 router.get('/new', (req,res)=>{
   res.render('books/new.ejs')
   });
 
-
-//1.2
+  //1.2
 router.post('/', (req,res)=>{
   Book.create(req.body, (err, createdBook)=>{
     if(err){
       console.log(err);
     }else{
       res.redirect('/books')
+    }
+  })
+});
+
+//1.3 
+router.get('/:id', (req,res)=>{
+  Book.findById(req.params.id, (err, foundBook)=>{
+    if(err){
+      res.send(err)
+    }else{
+      res.render('books/show.ejs',{
+        book: foundBook
+      })
     }
   })
 });
