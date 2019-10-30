7af7143d229a4713a42d900d039d35fb67ea1232
Eder Canada <canada.eder@gmail.com>
2019-10-30 14:04:00 -0700

    require user on bookscontroller

--- controllers/books.js
+++ controllers/books.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../models/books.js');
+const User = require('../models/users.js');
 
 //1.0
 router.get('/', async (req,res)=>{
