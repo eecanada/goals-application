c332506d2b547fcc5405cb8b7d517315a920cf16
Eder Canada <canada.eder@gmail.com>
2019-10-30 13:58:06 -0700

    create relationship betwwen user and book

--- models/users.js
+++ models/users.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
-})
+  books: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Book'
+  }]
+});
 
 const User = mongoose.model('User', userSchema);
 
