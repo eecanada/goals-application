7d05e9610a4e322b8ca6a7efa770e2dd0115fd13
Eder Canada <canada.eder@gmail.com>
2019-10-30 21:34:08 -0700

    require book on user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users.js');
-
+const Book = require('../models/books.js');
 //1.0
 router.get('/', async (req,res)=>{
   try{
